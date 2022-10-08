a393cfab2d9fe3ad4873ce1b73694d62d46d12ec
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-10-08 11:31:30 -0300

    pratica de menu responsiva feita

--- praticando-28/js/script.js
+++ praticando-28/js/script.js
@@ -1,8 +1,19 @@
-
 const btnMenu = document.querySelector('.btn-open');
 const Menu = document.querySelector('.lista-menu');
 
+function mudouTamanho() {
+    if (window.innerWidth <= 768) {
+
+        btnMenu.classList.remove('active');
+        
+        Menu.classList.remove('active');
+    }
+}
+
 btnMenu.addEventListener('click', () => {
+
     btnMenu.classList.toggle('active');
-    Menu.classList.toggle('active')
+
+    Menu.classList.toggle('active');
+
 })
