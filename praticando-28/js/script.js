6c2dd014f3d30767c6be57d5a00badb7cfc4ad3d
Kauã da Silva Andrade <107755487+kauasilvandrade@users.noreply.github.com>
2022-10-14 16:04:21 -0300

    pratica feita

--- praticando-28/js/script.js
+++ praticando-28/js/script.js
@@ -1,5 +1,6 @@
 const btnMenu = document.querySelector('.btn-open');
 const Menu = document.querySelector('.lista-menu');
+const btnClose = document.querySelector('.x-hamburguer')
 
 function mudouTamanho() {
     if (window.innerWidth <= 768) {
@@ -12,8 +13,15 @@ function mudouTamanho() {
 
 btnMenu.addEventListener('click', () => {
 
-    btnMenu.classList.toggle('active');
-
     Menu.classList.toggle('active');
+    
+    btnClose.classList.toggle('active');
 
 })
+
+function fecharMenu() {
+    if (Menu.classList.contains('active'), btnClose.classList.contains('active')) {
+        Menu.classList.remove('active');
+        btnClose.classList.remove('active');
+    }
+}
